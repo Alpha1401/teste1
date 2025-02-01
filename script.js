6a0acf3e3844266a9e58801424fae66e62c5cc63
Alpha1401 <otavioskate9@gmail.com>
2025-01-31 20:38:00 -0800

    Atualizando pastas e corrigindo links

--- script.js
+++ script.js
@@ -1,117 +1,117 @@
-// video 1
+// // video 1
 
-document.getElementById("playVideo").addEventListener("click", function () {
-  const modal = document.getElementById("videoModal");
-  const video = document.getElementById("skillsVideo");
+// document.getElementById("playVideo").addEventListener("click", function () {
+//   const modal = document.getElementById("videoModal");
+//   const video = document.getElementById("skillsVideo");
   
-  // Exibe o modal e inicia o vídeo
-  modal.style.display = "flex";
-  video.play();
-});
+//   // Exibe o modal e inicia o vídeo
+//   modal.style.display = "flex";
+//   video.play();
+// });
 
-document.getElementById("closeModal").addEventListener("click", function () {
-  const modal = document.getElementById("videoModal");
-  const video = document.getElementById("skillsVideo");
+// document.getElementById("closeModal").addEventListener("click", function () {
+//   const modal = document.getElementById("videoModal");
+//   const video = document.getElementById("skillsVideo");
 
-  // Oculta o modal e pausa o vídeo
-  modal.style.display = "none";
-  video.pause();
-});
+//   // Oculta o modal e pausa o vídeo
+//   modal.style.display = "none";
+//   video.pause();
+// });
 
-// video 2
-document.getElementById("playVideo2").addEventListener("click", function () {
-  const modal = document.getElementById("videoModal2");
-  const video = document.getElementById("skillsVideo2");
+// // video 2
+// document.getElementById("playVideo2").addEventListener("click", function () {
+//   const modal = document.getElementById("videoModal2");
+//   const video = document.getElementById("skillsVideo2");
   
-  // Exibe o modal e inicia o vídeo
-  modal.style.display = "flex";
-  video.play();
-});
+//   // Exibe o modal e inicia o vídeo
+//   modal.style.display = "flex";
+//   video.play();
+// });
 
-document.getElementById("closeModal2").addEventListener("click", function () {
-  const modal = document.getElementById("videoModal2");
-  const video = document.getElementById("skillsVideo2");
+// document.getElementById("closeModal2").addEventListener("click", function () {
+//   const modal = document.getElementById("videoModal2");
+//   const video = document.getElementById("skillsVideo2");
 
-  // Oculta o modal e pausa o vídeo
-  modal.style.display = "none";
-  video.pause();
-});
+//   // Oculta o modal e pausa o vídeo
+//   modal.style.display = "none";
+//   video.pause();
+// });
 
-// video 3
-document.getElementById("playVideo3").addEventListener("click", function () {
-  const modal = document.getElementById("videoModal3");
-  const video = document.getElementById("skillsVideo3");
+// // video 3
+// document.getElementById("playVideo3").addEventListener("click", function () {
+//   const modal = document.getElementById("videoModal3");
+//   const video = document.getElementById("skillsVideo3");
   
-  // Exibe o modal e inicia o vídeo
-  modal.style.display = "flex";
-  video.play();
-});
+//   // Exibe o modal e inicia o vídeo
+//   modal.style.display = "flex";
+//   video.play();
+// });
 
-document.getElementById("closeModal3").addEventListener("click", function () {
-  const modal = document.getElementById("videoModal3");
-  const video = document.getElementById("skillsVideo3");
+// document.getElementById("closeModal3").addEventListener("click", function () {
+//   const modal = document.getElementById("videoModal3");
+//   const video = document.getElementById("skillsVideo3");
 
-  // Oculta o modal e pausa o vídeo
-  modal.style.display = "none";
-  video.pause();
-});
+//   // Oculta o modal e pausa o vídeo
+//   modal.style.display = "none";
+//   video.pause();
+// });
 
-// video 4
-document.getElementById("playVideo4").addEventListener("click", function () {
-  const modal = document.getElementById("videoModal4");
-  const video = document.getElementById("skillsVideo4");
+// // video 4
+// document.getElementById("playVideo4").addEventListener("click", function () {
+//   const modal = document.getElementById("videoModal4");
+//   const video = document.getElementById("skillsVideo4");
   
-  // Exibe o modal e inicia o vídeo
-  modal.style.display = "flex";
-  video.play();
-});
+//   // Exibe o modal e inicia o vídeo
+//   modal.style.display = "flex";
+//   video.play();
+// });
 
-document.getElementById("closeModal4").addEventListener("click", function () {
-  const modal = document.getElementById("videoModal4");
-  const video = document.getElementById("skillsVideo4");
+// document.getElementById("closeModal4").addEventListener("click", function () {
+//   const modal = document.getElementById("videoModal4");
+//   const video = document.getElementById("skillsVideo4");
 
-  // Oculta o modal e pausa o vídeo
-  modal.style.display = "none";
-  video.pause();
-});
+//   // Oculta o modal e pausa o vídeo
+//   modal.style.display = "none";
+//   video.pause();
+// });
 
-// video5
-document.getElementById("playVideo5").addEventListener("click", function () {
-  const modal = document.getElementById("videoModal5");
-  const video = document.getElementById("skillsVideo5");
+// // video5
+// document.getElementById("playVideo5").addEventListener("click", function () {
+//   const modal = document.getElementById("videoModal5");
+//   const video = document.getElementById("skillsVideo5");
   
-  // Exibe o modal e inicia o vídeo
-  modal.style.display = "flex";
-  video.play();
-});
+//   // Exibe o modal e inicia o vídeo
+//   modal.style.display = "flex";
+//   video.play();
+// });
 
-document.getElementById("closeModal5").addEventListener("click", function () {
-  const modal = document.getElementById("videoModal5");
-  const video = document.getElementById("skillsVideo5");
+// document.getElementById("closeModal5").addEventListener("click", function () {
+//   const modal = document.getElementById("videoModal5");
+//   const video = document.getElementById("skillsVideo5");
 
-  // Oculta o modal e pausa o vídeo
-  modal.style.display = "none";
-  video.pause();
-});
+//   // Oculta o modal e pausa o vídeo
+//   modal.style.display = "none";
+//   video.pause();
+// });
 
-// video 6
-document.getElementById("playVideo6").addEventListener("click", function () {
-  const modal = document.getElementById("videoModal6");
-  const video = document.getElementById("skillsVideo6");
+// // video 6
+// document.getElementById("playVideo6").addEventListener("click", function () {
+//   const modal = document.getElementById("videoModal6");
+//   const video = document.getElementById("skillsVideo6");
   
-  // Exibe o modal e inicia o vídeo
-  modal.style.display = "flex";
-  video.play();
-});
+//   // Exibe o modal e inicia o vídeo
+//   modal.style.display = "flex";
+//   video.play();
+// });
 
-document.getElementById("closeModal6").addEventListener("click", function () {
-  const modal = document.getElementById("videoModal6");
-  const video = document.getElementById("skillsVideo6");
+// document.getElementById("closeModal6").addEventListener("click", function () {
+//   const modal = document.getElementById("videoModal6");
+//   const video = document.getElementById("skillsVideo6");
 
-  // Oculta o modal e pausa o vídeo
-  modal.style.display = "none";
-  video.pause();
-});
+//   // Oculta o modal e pausa o vídeo
+//   modal.style.display = "none";
+//   video.pause();
+// });
 
 
 
